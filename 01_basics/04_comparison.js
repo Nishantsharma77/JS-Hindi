347234512ec6b97dcd8537d4ac134af1a5e546c9
Nishant Sharma <165388083+Nishantsharma77@users.noreply.github.com>
2025-07-29 19:19:15 +0000

    Comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,18 @@
+// console.log("2">1); //true
+// console.log("02">1);//true
+
+// console.log(null>0); //false
+// console.log(null==0);//false
+// console.log(null>=0);//true 
+
+//reason is that equality check and comparisons work differently. comparisons convert null to a number treating it as 0
+
+// console.log(undefined ==0);//always false
+
+// ===
+
+console.log("2"===2);
+
+
+ 
+
