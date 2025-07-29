af945f59e06d643802e382278bea01d7820e06d4
Nishant Sharma <165388083+Nishantsharma77@users.noreply.github.com>
2025-07-29 17:50:03 +0000

    container config done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict" //treat all js code as newer version
+
+//alert(3+3) //we are using nodejs, not browser
+
+console.log(3+3) 
+console.log("hitesh")
+
+
+let name = "Nishant"
+let age = 18
+let isLoggedIn = false
+
+//number => 2 to power 53 (range)
+//bigint
+//string=> ""
+//boolean =>true/false
+//null =>standalone value (khali hai)
+//undefined => value not assigned
+//symbol =>
+
+//object
+
