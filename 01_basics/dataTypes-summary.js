0a76da875431ca483ad5d1c19f41aaf36f36590b
Nishant Sharma <165388083+Nishantsharma77@users.noreply.github.com>
2025-07-30 06:39:27 +0000

    dataType-summary

---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,47 @@
+ //Primitive
+
+ // 7 types: String,Number, Boolean, null, undefined, Symbol, BigInt
+ 
+ // Js in a dynamically typd language
+
+ const score = 100
+ const scoreValue = 100.3
+
+ const isLoggedIn = false
+ const outsideTemp = null
+ let userEmail;
+
+ const id = Symbol('123')
+ const anotherId = Symbol('123')
+ console.log(id === anotherId);
+
+//  const bigNumber = 3349857052175n
+
+ //Refernce(Non Primitive)
+
+ //Array, Objects, Functions =>datatype=Function 
+
+ const heros = ["Shaktiman" , "naagraj"];
+
+ //objects
+
+ let myobj = {
+    name:"Nishant",
+    age : 22 
+ }
+
+ //function
+
+ const myFunction=function(){
+console.log("hello world");
+
+ }
+
+ //datatype pata kro
+ console.log(typeof outsideTemp);
+
+ //type of null is objct
+//  type of undefined is undefined
+//type of function is function
+
+ 
