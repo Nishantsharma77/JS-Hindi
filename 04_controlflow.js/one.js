020d3a8fa66903250fafcad7cd82bc736c1a9ea4
Nishant Sharma <165388083+Nishantsharma77@users.noreply.github.com>
2025-08-05 17:33:56 +0000

    if else basics

---
+++ 04_controlflow.js/one.js
@@ -0,0 +1,68 @@
+//if
+const isUserLoggedIn = true
+// const temperature = 41
+
+// if( temperature===60){
+//    console.log("less than 50"); 
+// }else{
+ 
+// console.log("tempratur greater than 50");
+// }
+
+// console.log("Executed");
+
+// const score = 200
+
+// if (score > 100) {
+//     const power = "fly"
+//     console.log(`user power : ${power}`);  
+// }
+//  console.log(`user power : ${power}`);
+
+
+
+// const balance = 1000
+
+// // if (balance > 500) console.log("test"),  console.log("test 2");  //implicit scope
+
+// if (balance < 500) {
+//     console.log("less than");
+// }else if(balance<750){
+//     console.log("less than 750");  
+// }
+// else{
+//     console.log("less than 1200");
+    
+// }
+
+
+
+
+const userLoggedIn = true
+const debitcard = true
+const loggedInFromGoogle = false
+const userLoggedInFromEmail = true
+
+if (userLoggedIn && debitcard && 2==3) {
+    console.log("ALLOW TO BUY COURSE");
+}
+if (loggedInFromGoogle || userLoggedInFromEmail) {
+    console.log("user logged in");
+    
+}
+
+
+    
+
+
+//  var ka scope completely global hai
+
+
+//
+// ==(equal to)
+
+//
+// ===(it also checks the type)
+
+
+
