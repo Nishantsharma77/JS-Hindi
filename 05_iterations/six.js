28087b039ceefcefbf162040a47d66a8dd248c77
Nishant Sharma <165388083+Nishantsharma77@users.noreply.github.com>
2025-08-24 08:40:56 +0000

    maps.js

---
+++ 05_iterations/six.js
@@ -0,0 +1,30 @@
+// const coding = ["js", "ruby", "java" , "python", "cpp"]
+// //for each values undefined deta hai
+// const values = coding.forEach( (item) => {
+// //  console.log(item);
+//  return item
+// })
+// console.log(values);
+
+
+//filter value vapis karta hai
+//callback function milega sbhbi values ko access kiya jayega lekin condition deni padti hai jo jo values satisfy kre
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+//this is good method
+// //scope mein hmesha return likhna hai
+// const newNums = myNums.filter( (num) => {
+//    return num > 4
+// })
+
+//for each
+// const newNums = []
+
+// myNums.forEach( (num) => {
+//     if (num>4) {
+//         newNums.push(num)
+//     }
+// })
+
+// console.log(newNums);
+
+
